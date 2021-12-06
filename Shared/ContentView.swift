157a19708cc019d3e5240377afd414127aea6f26
Yoonju Hwang <hwyj3464@gmail.com>
2021-12-06 13:17:55 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Yoonju Hwang on 12/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SwiftUIChatTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatTutorialApp.swift
+//  Shared
+//
+//  Created by Yoonju Hwang on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
